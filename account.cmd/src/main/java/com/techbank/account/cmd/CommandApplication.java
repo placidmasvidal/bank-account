fa1a714afee7076c04c8c9c619f7f383c1edea68
Placid Masvidal <contact@placidmasvidal.com>
2022-01-14 15:27:31 -0500

    Register Command Handlers Method coded.

--- account.cmd/src/main/java/com/techbank/account/cmd/CommandApplication.java
+++ account.cmd/src/main/java/com/techbank/account/cmd/CommandApplication.java
@@ -1,13 +1,34 @@
 package com.techbank.account.cmd;
 
+import com.techbank.account.cmd.api.commands.*;
+import com.techbank.cqrs.core.infrastructure.CommandDispatcher;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 public class CommandApplication {
+
+	private CommandDispatcher commandDispatcher;
+	private CommandHandler commandHandler;
+
+	@Autowired
+	public CommandApplication(CommandDispatcher commandDispatcher, CommandHandler commandHandler) {
+		this.commandDispatcher = commandDispatcher;
+		this.commandHandler = commandHandler;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CommandApplication.class, args);
 	}
 
+	@PostConstruct
+	public void registerHandlers() {
+		commandDispatcher.registerHandler(OpenAccountCommand.class, commandHandler::handle);
+		commandDispatcher.registerHandler(DepositFundsCommand.class, commandHandler::handle);
+		commandDispatcher.registerHandler(WithdrawFundsCommand.class, commandHandler::handle);
+		commandDispatcher.registerHandler(CloseAccountCommand.class, commandHandler::handle);
+	}
 }

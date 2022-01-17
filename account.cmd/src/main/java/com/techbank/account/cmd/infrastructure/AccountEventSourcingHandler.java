c54f385b84612f503072b4d2e771e1bd592451e8
Placid Masvidal <contact@placidmasvidal.com>
2022-01-17 07:06:56 -0500

    Restore the entire read db functionality implemented.

--- account.cmd/src/main/java/com/techbank/account/cmd/infrastructure/AccountEventSourcingHandler.java
+++ account.cmd/src/main/java/com/techbank/account/cmd/infrastructure/AccountEventSourcingHandler.java
@@ -4,6 +4,7 @@ import com.techbank.account.cmd.domain.AccountAggregate;
 import com.techbank.cqrs.core.domain.AggregateRoot;
 import com.techbank.cqrs.core.handlers.EventSourcingHandler;
 import com.techbank.cqrs.core.infrastructure.EventStore;
+import com.techbank.cqrs.core.producers.EventProducer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,10 +13,12 @@ import java.util.Comparator;
 @Service
 public class AccountEventSourcingHandler implements EventSourcingHandler<AccountAggregate> {
     private EventStore eventStore;
+    private EventProducer eventProducer;
 
     @Autowired
-    public AccountEventSourcingHandler(EventStore eventStore) {
+    public AccountEventSourcingHandler(EventStore eventStore, EventProducer eventProducer) {
         this.eventStore = eventStore;
+        this.eventProducer = eventProducer;
     }
 
     @Override
@@ -35,5 +38,18 @@ public class AccountEventSourcingHandler implements EventSourcingHandler<Account
             aggregate.setVersion(latestVersion.get());
         }
         return aggregate;
+    }
+
+    @Override
+    public void republishEvents() {
+        var aggregateIds = eventStore.getAggregateIds();
+    for (var aggregateId : aggregateIds) {
+      var aggregate = getById(aggregateId);
+        if (aggregate == null || !aggregate.getActive()) continue;
+        var events = eventStore.getEvents(aggregateId);
+        for (var event : events) {
+            eventProducer.produce(event.getClass().getSimpleName(), event);
+        }
+    }
     }
 }
